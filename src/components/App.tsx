a36aa1666963c2af29dd837eeec4bf5af3300c86
JonLacerdaDev <jonathanlacerdadev@outlook.com>
2024-03-20 17:19:28 -0300

    Configure base project

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import '../styles/main.scss';
+import CharacterList from './CharacterList';
+
+const App = () => {
+  return (
+    <div className="container">
+      <CharacterList />
+    </div>
+  );
+}
+
+export default App;
